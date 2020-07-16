0b6a9c30646d2bbc5946e82097618592ec32501c
이기준 <2stndard@gmail.com>
2020-07-17 00:34:47 +0900

    third commit

--- analysis.R
+++ analysis.R
@@ -1,25 +1,14 @@
 library(fpp2)
 
-if(!require(ggrepel)) {
-  install.packages('ggrepel')
-  library(ggrepel)
-}
-
 if(!require(ggthemes)) {
   install.packages('ggthemes')
   library(ggthemes)
 }
 
-library(ggrepel)
 library(RColorBrewer)
 
-install.packages('tinytex')
-tinytex::install_tinytex()
-
-install.packages("knitr")
-
-edu_svc <- read.csv('D:/R/data/교육서비스업취업자.csv', stringsAsFactors = F)
-tot.edu_svc <- read.csv('D:/R/data/교육서비스업취업자(전체).csv', stringsAsFactors = F)
+edu_svc <- read.csv('C:/R/data/교육서비스업취업자.csv', stringsAsFactors = F)
+tot.edu_svc <- read.csv('C:/R/data/교육서비스업취업자(전체).csv', stringsAsFactors = F)
 
 ts.edu_svc <- ts(edu_svc[, -1], start = c(2013, 1), frequency = 12)
 
@@ -27,7 +16,7 @@ ts.tot.edu_svc <- ts(tot.edu_svc[, -1], start = c(2013, 1), frequency = 12)
 
 autoplot(ts.tot.edu_svc[,1], series = '취업자수', size = 1) + 
   ggtitle('전체 취업자수') +
-  geom_point(color ='#40b8d0') +
+  geom_point(color ='grey50') +
   scale_x_continuous(breaks = seq(2013, 2020, 1)) + 
   xlab('연도') + ylab('취업자수(천명)') + 
   theme_economist() +
@@ -37,13 +26,16 @@ autoplot(ts.tot.edu_svc[,1], series = '취업자수', size = 1) +
         panel.grid.major = element_line(color = 'white'),
         panel.grid.minor = element_blank(), 
         panel.background = element_blank(), 
-        legend.position = 'bottom', 
-        legend.title = element_blank())
+        legend.position = 'none', 
+        legend.title = element_blank(),
+        axis.title.y = element_text(size = rel(1.5), angle = 90),
+        axis.title.x = element_text(size = rel(1.5)),
+        plot.title = element_text(size = 30))
 
 
 autoplot(ts.edu_svc, series = '취업자수', size = 1) + 
   ggtitle('교육서비스업 취업자수') +
-  geom_point(color ='#40b8d0') +
+  geom_point(color ='grey50') +
   scale_x_continuous(breaks = seq(2013, 2020, 1)) + 
   xlab('연도') + ylab('취업자수(천명)') + 
   theme_economist() +
@@ -53,13 +45,16 @@ autoplot(ts.edu_svc, series = '취업자수', size = 1) +
         panel.grid.major = element_line(color = 'white'),
         panel.grid.minor = element_blank(), 
         panel.background = element_blank(), 
-        legend.position = 'bottom', 
-        legend.title = element_blank())
+        legend.position = 'none', 
+        legend.title = element_blank(),
+        axis.title.y = element_text(size = rel(1.5), angle = 90),
+        axis.title.x = element_text(size = rel(1.5)),
+        plot.title = element_text(size = 30))
 
 
 ggseasonplot(ts.tot.edu_svc[,1], series = '취업자수', size = 1, year.labels = T) + 
   scale_color_brewer(palette = "Dark2") +
-  ggtitle('전체 취업자수') +
+  ggtitle('월별 전체 취업자수') +
   geom_point() + 
   xlab('월') + ylab('취업자수(천명)') +
   theme_economist() +
@@ -68,13 +63,16 @@ ggseasonplot(ts.tot.edu_svc[,1], series = '취업자수', size = 1, year.labels 
         panel.grid.major = element_line(color = 'white'),
         panel.grid.minor = element_blank(), 
         panel.background = element_blank(), 
-        legend.position = 'bottom', 
-        legend.title = element_blank())
+        legend.position = 'none', 
+        legend.title = element_blank(),
+        axis.title.y = element_text(size = rel(1.5), angle = 90),
+        axis.title.x = element_text(size = rel(1.5)),
+        plot.title = element_text(size = 30))
 
 
 ggseasonplot(ts.edu_svc, series = '취업자수', size = 1, year.labels = T) + 
   scale_color_brewer(palette = "Dark2") +
-  ggtitle('교육서비스업 취업자수') +
+  ggtitle('월별 교육서비스업 취업자수') +
   geom_point() + 
   scale_y_continuous(breaks = seq(1650, 2000, 50)) + 
   xlab('월') + ylab('취업자수(천명)') + 
@@ -84,14 +82,16 @@ ggseasonplot(ts.edu_svc, series = '취업자수', size = 1, year.labels = T) +
         panel.grid.major = element_line(color = 'white'),
         panel.grid.minor = element_blank(), 
         panel.background = element_blank(), 
-        legend.position = 'bottom', 
-        legend.title = element_blank())
+        legend.position = 'none', 
+        legend.title = element_blank(),
+        axis.title.y = element_text(size = rel(1.5), angle = 90),
+        axis.title.x = element_text(size = rel(1.5)),
+        plot.title = element_text(size = 30))
 
 ggsubseriesplot(ts.tot.edu_svc[,1], series = '취업자수', size = 1, year.labels = T) + 
   scale_color_brewer(palette = "Dark2") +
-  ggtitle('전체 취업자수') +
+  ggtitle('월별 전체 취업자수 추이') +
   geom_point() + 
-  scale_y_continuous(breaks = seq(1650, 2000, 50)) + 
   xlab('월') + ylab('취업자수(천명)') + 
   theme_economist() +
   theme(axis.text.x = element_text(color = 'black', size = 10),
@@ -99,12 +99,15 @@ ggsubseriesplot(ts.tot.edu_svc[,1], series = '취업자수', size = 1, year.labe
         panel.grid.major = element_line(color = 'white'),
         panel.grid.minor = element_blank(), 
         panel.background = element_blank(), 
-        legend.position = 'bottom', 
-        legend.title = element_blank())
+        legend.position = 'none', 
+        legend.title = element_blank(),
+        axis.title.y = element_text(size = rel(1.5), angle = 90),
+        axis.title.x = element_text(size = rel(1.5)),
+        plot.title = element_text(size = 30))
 
 ggsubseriesplot(ts.edu_svc, series = '취업자수', size = 1, year.labels = T) + 
   scale_color_brewer(palette = "Dark2") +
-  ggtitle('교육서비스업 취업자수') +
+  ggtitle('월별 교육서비스업 취업자수 추이') +
   geom_point() + 
   scale_y_continuous(breaks = seq(1650, 2000, 50)) + 
   xlab('월') + ylab('취업자수(천명)') + 
@@ -114,16 +117,46 @@ ggsubseriesplot(ts.edu_svc, series = '취업자수', size = 1, year.labels = T) 
         panel.grid.major = element_line(color = 'white'),
         panel.grid.minor = element_blank(), 
         panel.background = element_blank(), 
-        legend.position = 'bottom', 
-        legend.title = element_blank())
+        legend.position = 'none', 
+        legend.title = element_blank(),
+        axis.title.y = element_text(size = rel(1.5), angle = 90),
+        axis.title.x = element_text(size = rel(1.5)),
+        plot.title = element_text(size = 30))
 
-t <- as.data.frame(cbind(rep(2013:2020, each = 12)[-(91:96)], as.vector(month.abb[cycle(ts.edu_svc)]), as.vector(ts.edu_svc)))
-t[,3] <- as.numeric(t[,3])
+?ggsubseriesplot
+t <- as.data.frame(cbind(rep(2013:2020, each = 12)[-(91:96)], as.vector(month.abb[cycle(ts.edu_svc)]), as.numeric(ts.edu_svc)))
+
+nrow(t)
+as.numeric(as.character(t[,3]))
+t[,3] <- as.numeric(as.character(t[,3]))
+
 t[,2] <- factor(t[,2], levels = c('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'), labels = c('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'), ordered = T)
 colnames(t) <- c('year', 'month', 'data')
-
-ggplot(t, aes(x = year, y = data)) + geom_line(aes(group = month)) + facet_wrap(~month) + 
+t
+library(tidyverse)
+t %>% filter(month == 'Jun') %>% summarise(median(data))
+t %>% filter(month == 'Jun') %>% summarise(mean(data))
+?median
+ggplot(t, aes(x = year, y = data)) + geom_line(aes(group = month)) + facet_wrap(~month)+ 
   scale_x_discrete(breaks = seq(2013, 2020, 1), labels = c('13', '14', '15', '16', '17', '18', '19', '20')) +
+  geom_point(color = 'grey70') + 
+  ggtitle('월별 교육서비스업 취업자수 추이') +
+  xlab('연도') + ylab('취업자수(천명)') + 
+  theme_economist() +
+  theme(axis.text.x = element_text(color = 'black', size = 10),
+        axis.text.y = element_text(color = 'black', size = 10), 
+        panel.grid.major = element_line(color = 'white'),
+        panel.grid.minor = element_blank(), 
+        panel.background = element_blank(), 
+        legend.position = 'none', 
+        legend.title = element_blank(),
+        axis.title.y = element_text(size = rel(1.5), angle = 90),
+        axis.title.x = element_text(size = rel(1.5)),
+        plot.title = element_text(size = 30))
+mean(t[t$year == '2018', 3])
+ggplot(t, aes(x = month, y = data)) + geom_line(aes(group = year)) + facet_wrap(~year, nrow = 3, ncol = 3) + 
+  ggtitle('연도별 교육서비스업 취업자수 추이') +
+  scale_x_discrete(labels = c('1','2','3','4','5','6','7','8','9','10','11','12')) +
   geom_point(color = 'grey70') + 
   xlab('연도') + ylab('취업자수(천명)') + 
   theme_economist() +
@@ -132,22 +165,12 @@ ggplot(t, aes(x = year, y = data)) + geom_line(aes(group = month)) + facet_wrap(
         panel.grid.major = element_line(color = 'white'),
         panel.grid.minor = element_blank(), 
         panel.background = element_blank(), 
-        legend.position = 'bottom', 
-        legend.title = element_blank())
+        legend.position = 'none', 
+        legend.title = element_blank(),
+        axis.title.y = element_text(size = rel(1.5), angle = 90),
+        axis.title.x = element_text(size = rel(1.5)),
+        plot.title = element_text(size = 30))
 
-ggplot(t, aes(x = month, y = data)) + geom_line(aes(group = year)) + facet_wrap(~year, nrow = 3, ncol = 3) + 
-  scale_x_discrete(labels = c('1','2','3','4','5','6','7','8','9','10','11','12')) +
-  geom_point(color = 'grey70') + 
-  xlab('연도') + ylab('취업자수(천명)') + 
-  theme(axis.text.x = element_text(color = 'black', size = 10),
-        axis.text.y = element_text(color = 'black', size = 10), 
-        panel.grid.major = element_line(color = 'black'),
-        panel.grid.minor = element_line(color = 'black'),
-        panel.background = element_blank(),
-        legend.position = 'bottom',
-        legend.title = element_blank()) + 
-  theme_economist()
-  
 diff(ts.edu_svc, 1)
 diff(ts.edu_svc, 12)
 
